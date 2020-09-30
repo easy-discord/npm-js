90abd3cf25ae912c1ca90ddea0fe719771d45e4f
Seer <50992478+killyaskill@users.noreply.github.com>
2020-09-30 19:08:06 +0200

    actual package...

---
+++ core-projz.js
@@ -0,0 +1,8 @@
+module.exports = {
+    hello: function(subject) {
+        console.log('Jupiter says hello ' + subject);
+      },
+    hw: function(subject) {
+    console.log('Jupiter says hello ' + subject);
+    }
+  };
