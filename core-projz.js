7bc9c570c7ff787cc252652e212817aabd02ac9f
Seer <50992478+killyaskill@users.noreply.github.com>
2020-09-30 19:34:12 +0200

    GET

--- core-projz.js
+++ core-projz.js
@@ -1,7 +1,22 @@
-module.exports = { // test commands...
+const https = require('https');
+
+module.exports = {
     get: function(subject) {
         if(subject == 'regions'){
-            console.log('Reeegiooons!');
+            https.get('https://api.projz.com/v1/configs/content-regions', (resp) => {
+                let data = '';
+
+               
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                resp.on('end', () => {
+                    console.log(JSON.parse(data).countryCode);
+                });
+
+                }).on("error", (err) => {
+                console.log("Error: " + err.message);
+                });
         }
       },
     hw: function(subject) {
