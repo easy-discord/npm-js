b2e3b9529de945ff22fec9ffae68b150f7736d19
Seer <50992478+killyaskill@users.noreply.github.com>
2020-10-01 18:49:31 +0200

    Add actual functioning commands...

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Easy Discord
+An easy way to make discord bots!
+
+## How to install?
+```
+npm i @dxy_seer/easydiscord
+```
+## How to use?
+
+Use the following code to use the library!
+This library will be updated every so often!
+
+### Starter code
+```
+const cli = require('@dxy_seer/easydiscord');
+var prefix = 'ed!';
+var token = 'Secret token!';
+
+cli.log('Yes');
+cli.send('channel', `${prefix}hi`, 'Hello!');
+
+
+cli.login(token);
+```
+#### Options
+cli.log('Hello world') 
+Will display: ED -> Hello world
+
+cli.login(token) 
+Will display: ED -> Bot online! (Check discord if the bot is online!)
+
+cli.send('channel', `${prefix}hi`, 'hello!');
+Will display: hello! (In a discord channel where **${prefix}hi** was used)
+
+cli.send('reply', `${prefix}hi`, 'hello!');
+Will display: @user, hello! (In a discord channel where **${prefix}hi** was used)
--- core-easy-dis.js
+++ core-easy-dis.js
@@ -1,12 +1,45 @@
-
-
+const discord = require('discord.js');
+const bot = new discord.Client();
+var modulename = "ED";
 module.exports = {
     get: function(arg) {
-        if(arg == 'regions'){
 
-        }
       },
+      // log 
     log: function(arg) {
-    console.log(arg);
+        console.log(`${modulename} -> ` + arg);
+    },
+    // log //
+    // login
+    login: function(token){
+      if(token == ''){
+        this.log(`Invalid token...`);
+      } else {
+        bot.login(token);
+        this.log(`Bot online!`);
+      } 
+    },
+    // login //
+    // message 
+    send: function(type, activator, content) {
+        if(type == ''){
+          this.log('Please select a type! (channel/reply)');
+        } else if(content == ''){
+          this.log(`I cant send an empty message!`);
+        } else if(activator == '') {
+          this.log('No command defined');
+        }else {
+          bot.on("message", function(message) {
+          if (message.content === activator) {
+            if(type == 'reply'){
+              message.reply(content);
+            } 
+            if(type == 'channel'){
+              message.channel.send(content);
+            }
+          }
+          });
+        }
     }
+    
   };
